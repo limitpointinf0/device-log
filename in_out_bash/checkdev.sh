887d88c2a3392aeeca993d3f266f3428caef7dd5
limitpointinf0 <coneal0523@protonmail.com>
2018-06-16 01:09:22 -0700

    changed timestamp to UTC

--- in_out_bash/checkdev.sh
+++ in_out_bash/checkdev.sh
@@ -5,11 +5,11 @@ sleep_t="$2"
 while [ true ]; do
 command="$(arp-scan -l | grep "\<$name\>")"
 if [ "$command" != "" ]; then
-printf "$(date), in\n" >> $name.log
-printf "$(date), in\n"
+printf "$(date -u), in\n" >> $name.log
+printf "$(date -u), in\n"
 else
-printf "$(date), out\n" >> $name.log
-printf "$(date), out\n"
+printf "$(date -u), out\n" >> $name.log
+printf "$(date -u), out\n"
 fi
 sleep $sleep_t
 done 
